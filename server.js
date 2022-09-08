4c52f2be2087b4cf0caafbd1f5369e5401512d71
unknown <tobiburrohman2@gmail.com>
2022-09-08 15:59:35 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,11 +9,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
 // user: portfolio
-// paswd: 4m0WXXMJiNTZCPHh
-const { MongoClient, ServerApiVersion } = require("mongodb");
+// password: GoUYO0MogHl8NePW
+
+
 const uri =
-  "mongodb+srv://portfolio:4m0WXXMJiNTZCPHh@cluster0.hc4xz.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://portfolio:GoUYO0MogHl8NePW@cluster0.hc4xz.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -32,11 +38,17 @@ async function run() {
     });
 
     // load blogs
-    app.get("/blogs", async (req, res) => {
-        const result = await blogsCollection.find({}).toArray();
-        res.send(result);
-      });
+    // app.get("/blogs", async (req, res) => {
+    //     const result = await blogsCollection.find({}).toArray();
+    //     res.send(result);
+    //   });
 
+
+    // 1.a => load all user
+    // app.get("/user",   async (req, res) => {
+    //     const result = await usersCollection.find({}).toArray();
+    //     res.send(result);
+    //   });
 
 
 
@@ -46,9 +58,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

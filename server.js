df149ec2d38848168b0388ccbe81de9ef1d763cb
unknown <tobiburrohman2@gmail.com>
2022-09-18 12:49:38 +0600

    post api is done

--- server.js
+++ server.js
@@ -75,47 +75,19 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const usersCollection = client.db("portfolio").collection("users");
     const projectsCollection = client.db("portfolio").collection("projects");
     const blogsCollection = client.db("portfolio").collection("blogs");
-    const usersCollection = client.db("portfolio").collection("users");
     const photosCollection = client.db("portfolio").collection("photo");
 
-    // for file upload with the help of multer
-    app.post(
-      "/upload-files",
-      upload.single("photo"),
-      async (req, res, next) => {
-        const file = req.file;
-        if (!file) {
-          const error = new Error("Please upload a file");
-          error.httpStatusCode = 400;
-          return next(error);
-        }
-        const result = await photosCollection.insertOne(file);
-        res.send(result);
-      }
-    );
 
-    // load al upload-files data
-    app.get("/upload-files", async (req, res) => {
-      const result = await photosCollection.find().toArray();
-      res.send(result);
-    });
+    // verify an ADMIN
+    const verifyAdmin = async(req, res, next)=>{
+      const requester = req.decoded.email;
+      console.log('requester', requester);
+    }
 
-    // get images by id
-    app.get("/upload-files/:id([0-9a-fA-F]{24})", async (req, res) => {
-      const id = req.params.id;
-      console.log("id", id);
-      const query = { _id: ObjectId(id) };
-      const result = await photosCollection.findOne(query);
-      res.send(result)
-    });
-
-
-    // server png image
-    app.get("/image", (req, res) => {
-      res.sendFile(path.join(__dirname, "./uploads/tamiz--1662997746605.jpg"));
-    });
+   
 
     // 1.a => load all user
     app.get("/user", async (req, res) => {
@@ -124,39 +96,107 @@ async function run() {
     });
 
     // send user info in db
-    app.post("/user", async (req, res) => {
-      const user = req.body;
-      const email = "tobiburrohman2@gmail.com";
-      const password = "topu1842000";
-      if (user.email === email && user.password === password) {
-        const result = await usersCollection.insertOne(user);
-        res.send({ code: 200, result });
-      } else {
-        res.send({ code: 400 });
-      }
-    });
+    // app.post("/user", async (req, res) => {
+    //   const user = req.body;
+    //   const email = "tobiburrohman2@gmail.com";
+    //   const password = "topu1842000";
+    //   if (user.email === email && user.password === password) {
+    //     const result = await usersCollection.insertOne(user);
+    //     res.send({ code: 200, result });
+    //   } else {
+    //     res.send({ code: 400 });
+    //   }
+    // });
 
     // load project
-    app.get("/projects", async (req, res) => {
-      const result = await projectsCollection.find().toArray();
-      res.send(result);
-    });
+    // app.get("/projects", async (req, res) => {
+    //   const result = await projectsCollection.find().toArray();
+    //   res.send(result);
+    // });
 
-    // load blogs
-    app.get("/blogs", async (req, res) => {
+    // // load blogs
+    // app.get("/blogs", async (req, res) => {
+    //   const result = await blogsCollection.find({}).toArray();
+    //   res.send(result);
+    // });
+
+    // // load a single blog by _id
+    // app.get("/blogs/:id([0-9a-fA-F]{24})", async (req, res)=>{
+    //   const id = req.params.id;
+    //   // console.log('id', id)
+    //   const query = { _id: ObjectId(id) };
+    //   // console.log('query', query)
+    //   const result = await blogsCollection.findOne(query);
+    //   res.send(result)
+    // })  
+    
+    
+    //  // for file upload with the help of multer
+    //  app.post(
+    //   "/upload-files",
+    //   upload.single("photo"),
+    //   async (req, res, next) => {
+    //     const file = req.file;
+    //     if (!file) {
+    //       const error = new Error("Please upload a file");
+    //       error.httpStatusCode = 400;
+    //       return next(error);
+    //     }
+    //     const result = await photosCollection.insertOne(file);
+    //     res.send(result);
+    //   }
+    // );
+
+    // // load al upload-files data
+    // app.get("/upload-files", async (req, res) => {
+    //   const result = await photosCollection.find().toArray();
+    //   res.send(result);
+    // });
+
+    // // get images by id
+    // app.get("/upload-files/:id([0-9a-fA-F]{24})", async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log("id", id);
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await photosCollection.findOne(query);
+    //   res.send(result)
+    // });
+
+
+    // // server png image
+    // app.get("/image", (req, res) => {
+    //   res.sendFile(path.join(__dirname, "./uploads/tamiz--1662997746605.jpg"));
+    // });
+
+    // 3.a => get all blogs
+    app.get("/blogs", async(req, res)=>{
       const result = await blogsCollection.find({}).toArray();
       res.send(result);
-    });
+    })
 
-    // load a single blog by _id
-    app.get("/blogs/:id([0-9a-fA-F]{24})", async (req, res)=>{
+    // 3.b => post article in server to db
+    app.post("/blogs", async(req, res)=>{
+      const data = req.body;
+      const result = await blogsCollection.insertOne(data);
+      res.send(result);
+    })
+
+    // 3.c => get blogs load by id
+    app.get("/blogs/:id", async(req, res)=>{
       const id = req.params.id;
-      // console.log('id', id)
       const query = { _id: ObjectId(id) };
-      // console.log('query', query)
       const result = await blogsCollection.findOne(query);
-      res.send(result)
-    })   
+      res.send(result);
+    })
+
+    // 3.d => delete blog by id
+    app.delete("/blogs/:id", async (req, res)=>{
+      const id = req.params.id;
+      console.log('id', id)
+      const filter = {_id:ObjectId(id)}
+      const result = await blogsCollection.deleteOne(filter);
+      res.send(result);
+    })
     
   } finally {
     // await client.close();

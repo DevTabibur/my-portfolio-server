c8632f0fc297cc87008248115491d18194495b0b
unknown <tobiburrohman2@gmail.com>
2022-09-21 10:27:34 +0600

    all done

--- server.js
+++ server.js
@@ -81,88 +81,12 @@ async function run() {
     const photosCollection = client.db("portfolio").collection("photo");
 
 
-    // verify an ADMIN
-    const verifyAdmin = async(req, res, next)=>{
-      const requester = req.decoded.email;
-      console.log('requester', requester);
-    }
-
-   
-
     // 1.a => load all user
     app.get("/user", async (req, res) => {
       const result = await usersCollection.find({}).toArray();
       res.send(result);
     });
 
-    // send user info in db
-    // app.post("/user", async (req, res) => {
-    //   const user = req.body;
-    //   const email = "tobiburrohman2@gmail.com";
-    //   const password = "topu1842000";
-    //   if (user.email === email && user.password === password) {
-    //     const result = await usersCollection.insertOne(user);
-    //     res.send({ code: 200, result });
-    //   } else {
-    //     res.send({ code: 400 });
-    //   }
-    // });
-
-    
-
-    // // load blogs
-    // app.get("/blogs", async (req, res) => {
-    //   const result = await blogsCollection.find({}).toArray();
-    //   res.send(result);
-    // });
-
-    // // load a single blog by _id
-    // app.get("/blogs/:id([0-9a-fA-F]{24})", async (req, res)=>{
-    //   const id = req.params.id;
-    //   // console.log('id', id)
-    //   const query = { _id: ObjectId(id) };
-    //   // console.log('query', query)
-    //   const result = await blogsCollection.findOne(query);
-    //   res.send(result)
-    // })  
-    
-    
-    //  // for file upload with the help of multer
-    //  app.post(
-    //   "/upload-files",
-    //   upload.single("photo"),
-    //   async (req, res, next) => {
-    //     const file = req.file;
-    //     if (!file) {
-    //       const error = new Error("Please upload a file");
-    //       error.httpStatusCode = 400;
-    //       return next(error);
-    //     }
-    //     const result = await photosCollection.insertOne(file);
-    //     res.send(result);
-    //   }
-    // );
-
-    // // load al upload-files data
-    // app.get("/upload-files", async (req, res) => {
-    //   const result = await photosCollection.find().toArray();
-    //   res.send(result);
-    // });
-
-    // // get images by id
-    // app.get("/upload-files/:id([0-9a-fA-F]{24})", async (req, res) => {
-    //   const id = req.params.id;
-    //   console.log("id", id);
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await photosCollection.findOne(query);
-    //   res.send(result)
-    // });
-
-
-    // // server png image
-    // app.get("/image", (req, res) => {
-    //   res.sendFile(path.join(__dirname, "./uploads/tamiz--1662997746605.jpg"));
-    // });
 
     // 2.a => load project
     app.get("/projects", async (req, res) => {
